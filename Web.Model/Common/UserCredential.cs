ba961c823061b850bcb7d81cca66c0945ca2575f
Ahmad Amjad <79445941+ahmad-hawklogix001@users.noreply.github.com>
2021-11-23 12:29:42 +0500

    Deleting all files

--- Web.Model/Common/UserCredential.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Web.Model.Common
-{
-    public class UserCredential
-    {
-        public string username { get; set; }
-        public string password { get; set; }
-    }
-}
--- Web.Model/Response/BaseResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Web.Model
-{
-    public class BaseResponse
-    {
-        public bool Success { get; set; }
-        public string Message { get; set; }
-        public object Data { get; set; }
-    }
-}
