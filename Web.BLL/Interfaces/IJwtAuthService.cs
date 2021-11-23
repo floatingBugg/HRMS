4645dcc8ba97d42b53575a7047b9ed144b8f15a2
HamzaashiqMehmood46 <hamza@hawklogix.com>
2021-11-23 11:57:12 +0500

    Adding repository first time of hrms

---
+++ Web.BLL/Interfaces/IJwtAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Web.Model.Common;
+namespace Web_API
+{
+    public interface IJwtAuthService
+    {
+        string Authentication(UserCredential login);
+
+        string Register(RegisterCredential register);
+    }
+}
---
+++ Web.Model/Common/UserCredential.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.Model.Common
+{
+    public class UserCredential
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
