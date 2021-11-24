68936f3ee1ea85c27e039170e89e21874f0c8936
HamzaashiqMehmood46 <hamza@hawklogix.com>
2021-11-24 15:28:41 +0500

    Updation Adding EmployeeControllerr,EmployeeResopository,Interface,Employee Service

---
+++ Web.Services/Interfaces/IEmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Services.Interfaces
+{
+    public interface IEmployeeService
+    {
+        string CreateEmployee();
+
+        string UpdateEmployee();
+
+        string GetAllEmployee();
+    }
+}
