eae1bb48d7712931b0153c5a3b41581d43efe793
Zaibyteror <94899984+Zaibyteror@users.noreply.github.com>
2021-12-01 19:19:44 +0500

    Interfaces & concrete classes

--- Web.Data/Concrete/HRMSIMSAssetsRepository.cs
+++ Web.Data/Concrete/HRMSIMSAssetsRepository.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Web.Data.Db_Context;
+using Web.Data.Generic_Repository;
+using Web.Data.Interfaces;
+using Web.Data.Models;
 
 namespace Web.Data.Concrete
 {
-    public class HRMSIMSAssetsRepository
+    public class HRMSIMSAssetsRepository : GenericRepository<ImsTblAssests>, IHRMSIMSAssetsRepository
     {
+        public HRMSIMSAssetsRepository(DbHRMSContext context)
+            : base(context)
+        {
 
+        }
     }
+
 }
+
--- Web.Data/Interfaces/IHRMSIMSAssetsCategoryRepository.cs
+++ Web.Data/Interfaces/IHRMSIMSAssetsCategoryRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Web.Data.Generic_Repository;
+using Web.Data.Models;
 
 namespace Web.Data.Interfaces
 {
-    public interface IHRMSIMSAssetsCategoryRepository
+    public interface IHRMSIMSAssetsCategoryRepository: IRepository<ImsTblAssetsCategory>
     {
 
     }
