5b6ecdd0c8c960446d6389dbc4571ac93478438e
HamzaashiqMehmood46 <hamza@hawklogix.com>
2021-12-01 19:45:18 +0500

    updating

--- Web.Services/Concrete/InventoryService.cs
+++ Web.Services/Concrete/InventoryService.cs
@@ -1,13 +1,46 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Web.Data;
+using Web.Data.Models;
+using Web.Model;
 using Web.Services.Interfaces;
 
 namespace Web.Services.Concrete
 {
-    public class InventoryService: IInventoryService
+    public class InventoryService : IInventoryService
     {
+        IConfiguration _config;
+        private readonly IUnitOfWork _uow;
+
+        public InventoryService(IConfiguration config, , IUnitOfWork uow)
+        {
+            _config = config;
+            
+            _uow = uow;
+        }
+
+        public BaseResponse CreateAssets(ImsTblAssests assests)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BaseResponse DeleteEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BaseResponse GetAllEmployee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BaseResponse UpdateEmployee(ImsTblAssests assests)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Web.Services/Interfaces/IInventoryService.cs
+++ Web.Services/Interfaces/IInventoryService.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Web.Data.Models;
+using Web.Model;
 
 namespace Web.Services.Interfaces
 {
     public interface IInventoryService
     {
 
+        BaseResponse CreateAssets(ImsTblAssests assests);
+
+        BaseResponse UpdateEmployee(ImsTblAssests assests);
+
+        BaseResponse GetAllEmployee();
+
+        BaseResponse DeleteEmployee(int id);
     }
 }
