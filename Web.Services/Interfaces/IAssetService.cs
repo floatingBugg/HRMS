317462c0bb55e14e0f54b84fd6f0b3842d21bc39
HamzaashiqMehmood46 <hamza@hawklogix.com>
2021-12-01 19:02:34 +0500

    Create IAssetService.cs

---
+++ Web.Services/Interfaces/IAssetService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Data.Models;
+using Web.Model;
+
+namespace Web.Services.Interfaces
+{
+    public interface IAssetService
+    {
+
+        BaseResponse CreateAssets(ImsTblAssests assests);
+
+        BaseResponse UpdateEmployee(ImsTblAssests assests);
+
+        BaseResponse GetAllEmployee();
+
+        BaseResponse DeleteEmployee(int id);
+    }
+}
