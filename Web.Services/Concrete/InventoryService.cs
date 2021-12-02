1e37d71901d9236ab99eb73c5f936d7474a74115
AshissJ <ashiss@hawklogix.com>
2021-12-02 18:31:05 +0500

    Update Assets Display

--- Web.Services/Concrete/InventoryService.cs
+++ Web.Services/Concrete/InventoryService.cs
@@ -20,12 +20,15 @@ namespace Web.Services.Concrete
         IConfiguration _config;
         private readonly IUnitOfWork _uow;
 
-        public InventoryService(IConfiguration config, IHRMSIMSAssetsRepository hrmsassetsRepository, IUnitOfWork uow)
+        public InventoryService(IConfiguration config, IHRMSIMSAssetsRepository hrmsassetsRepository,IHRMSIMSAssetsCategoryRepository hrmsassetscategoryRepository, IUnitOfWork uow)
         {
+            _hrmsassetscategoryRepository = hrmsassetscategoryRepository;
             _config = config;
             _hrmsassetsRepository = hrmsassetsRepository;
             _uow = uow;
         }
+
+
 
 
         public BaseResponse CreateAssests(ImsTblAssetsCategory assests)
