c6e1d5eaa03417b09dfe020bc8171e4ab1a40a48
Zaibyteror <94899984+Zaibyteror@users.noreply.github.com>
2021-12-10 12:05:32 +0500

    Concrete classes

---
+++ Web.Data/Concrete/HRMSAssetCctvCamerasRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Data.Db_Context;
+using Web.Data.Generic_Repository;
+using Web.Data.Interfaces;
+using Web.Data.Models;
+
+namespace Web.Data.Concrete
+{
+    public class HRMSAssetCctvCamerasRepository : GenericRepository<ImsCctvCameras>, IHRMSIMSAssetCctvCamerasRepository
+    {
+
+        public HRMSAssetCctvCamerasRepository(DbHRMSContext context)
+              : base(context)
+        {
+
+        }
+
+
+    }
+}
