c9b9d442b549789c724f5f05ab1911dcf53bc5a2
Zaibyteror <94899984+Zaibyteror@users.noreply.github.com>
2021-12-10 12:43:08 +0500

    Create AssetController.cs

---
+++ Web.API/Controllers/AssetController.cs
@@ -0,0 +1,30 @@
+﻿using log4net.Repository.Hierarchy;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Web.Services.Interfaces;
+
+namespace Web.API.Controllers
+{
+    public class AssetController : Controller
+    {
+        private readonly IAssetService _assetservice;
+        private IConfiguration _config;
+        Logger _logger;
+        private IWebHostEnvironment _hostEnvironment;
+
+        public AssetController(IAssetService assetservice, IConfiguration config, IWebHostEnvironment environment)
+        {
+            _assetservice = assetservice;
+            _config = config;
+            _hostEnvironment = environment;
+            _logger = new Logger (_hostEnvironment);
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
