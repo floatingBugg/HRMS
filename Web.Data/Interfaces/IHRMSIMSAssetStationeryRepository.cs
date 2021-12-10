cf85426d3d55a2b51d7576843cde1208957f06b7
HamzaashiqMehmood46 <hamza@hawklogix.com>
2021-12-10 12:17:54 +0500

    Adding Interface Completed

---
+++ Web.Data/Interfaces/IHRMSIMSAssetStationeryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Web.Data.Generic_Repository;
+using Web.Data.Models;
+namespace Web.Data.Interfaces
+{
+    public interface IHRMSIMSAssetStationeryRepository : IRepository<ImsStationery> 
+    {
+    }
+}
