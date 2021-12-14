5c20e14e1b6802411c1fc1ceca7981aa07d67876
HamzaashiqMehmood46 <hamza@hawklogix.com>
2021-12-14 14:56:16 +0500

    updating

---
+++ Web.Services/Concrete/AssetAssignService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Data;
+using Web.Data.Interfaces;
+using Web.Services.Interfaces;
+
+namespace Web.Services.Concrete
+{
+    public class AssetAssignService : IAssetAssignService
+    {
+        private readonly IHRMSAssetAssignRepository _hrmsassetassignRepository;
+        IConfiguration _config;
+        private readonly IUnitOfWork _uow;
+        private readonly UnitOfWork unitofWork;
+
+        public AssetAssignService(IConfiguration config, IUnitOfWork uow, IHRMSAssetAssignRepository hrmsassetassignRepository)
+        {
+            _config = config;
+            _uow = uow;
+            
+            _hrmsassetassignRepository = hrmsassetassignRepository;
+        }
+
+
+    }
+}
