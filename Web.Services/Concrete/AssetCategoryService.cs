91c078ac54aad4e8edf03d6dc26d8726b5bd6678
Zaibyteror <94899984+Zaibyteror@users.noreply.github.com>
2021-12-15 18:20:17 +0500

    AssetCategory Controller & service

---
+++ Web.Services/Concrete/AssetCategoryService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Data;
+using Web.Data.Interfaces;
+using Web.Services.Interfaces;
+
+namespace Web.Services.Concrete
+{
+    public class AssetCategoryService : IAssetCategoryService
+    {
+        private readonly IHRMSAssetCategoryRepository _hrmsassetcategoryRepository;
+
+        IConfiguration _config;
+        private readonly IUnitOfWork _uow;
+        public AssetCategoryService(IConfiguration config, IHRMSAssetCategoryRepository hrmsassetcategoryRepository, IUnitOfWork uow)
+        {
+            _config = config;
+            _hrmsassetcategoryRepository = hrmsassetcategoryRepository;
+            _uow = uow;
+        }
+    }
+}
